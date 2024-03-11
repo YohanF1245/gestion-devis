f0741d63882674ad1acd9c1e1688b598bba3eac2
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-03-11 02:32:51 +0100

    login 1 of 3 vue

---
+++ smart_devis_index.php
@@ -0,0 +1,42 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="CSS/bs-color-override.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="min-vh-100">
+    <div class="container-fluid bg-pd-blue" style="height:15vh!important">
+</div>
+<div class="container mt-5">
+    <div class="row">
+        <div class="offset-4 col-4 d-flex justify-content-center">
+            <img class="img-fluid w-25" src="img/smart-devis-logo.svg">
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-4 offset-4">
+            <h2 class="text-center text-pd-yellow">BIENVENUE</h2>
+        </div>
+    </div>
+    <form class="col-4 offset-4">
+        <div class="mb-3">
+            <label for="userMail" class="form-label">Entrez votre e-mail</label>
+            <input type="email" class="form-control" id="userMail">
+        </div>
+        <div class="d-grid">
+            <button type="submit" class="btn btn-primary ">Envoyer</button>
+        </div>
+    </form>
+</div>
+<div class="container-fluid bg-pd-blue position-absolute bottom-0" style="height:15vh!important">
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
